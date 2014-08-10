57a349f09123a1c3e5e7710bab95841185e47f86
Toshiyuki Hina <thina@yourinventit.com>
2014-08-10 15:26:04 +0900

    Parses csv and json file.

--- app/controllers/api/v1/bulk/device_controller.rb
+++ app/controllers/api/v1/bulk/device_controller.rb
@@ -1,3 +1,6 @@
+require "csv"
+require "json"
+
 class Api::V1::Bulk::DeviceController < ApplicationController
   protect_from_forgery :except => :import
 
@@ -12,12 +15,28 @@ class Api::V1::Bulk::DeviceController < ApplicationController
     respond_to do |format|
       format.csv {
         Rails.logger.debug "* Uploaded with CSV format."
+        parse_csv(@tempfile_path)
         head :no_content 
       }
       format.json {
         Rails.logger.debug "* Uploaded with JSON format."
+        parse_json(@tempfile_path)
         head :no_content
       }
     end
   end
+
+  private
+
+  def parse_csv(filepath)
+    CSV.foreach(filepath) do |row|
+      Rails.logger.debug "#{row}"
+    end
+  end
+
+  def parse_json(filepath)
+    json = JSON.parse(File.read(filepath))
+    Rails.logger.debug "#{json}"
+  end
+
 end
